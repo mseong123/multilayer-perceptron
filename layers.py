7c52f6e7d2afe6ee7d296638c9b48d8db665a983
mseong <mseong123@hotmail.com>
2024-11-05 15:59:23 +0800

    mlp nearly done

--- layers.py
+++ layers.py
@@ -3,32 +3,40 @@ import numpy as np
 
 class DenseLayer():
     '''DenseLayer class with weight and bias attributes'''
-    def __init__(self, size:tuple|int, activate:str=None):
-        self._weight = np.array([])
-        self._bias = np.array([])
+    def __init__(self, size:tuple|int, activation:str|None = None):
+        self._weight:np.ndarray = np.array([])
+        self._bias:np.ndarray = np.array([])
         if isinstance(size, tuple):
-            self._size = size[0]
+            self._size:int = size[0]
         elif isinstance(size, int):
-            self._size = size
-        self._activate=activate
+            self._size:int = size
+        self._activation:str = activation
+        self._input_matrix:np.ndarray = np.array([])
     
-    def init_weight_bias(self, input:np.ndarray | None):
-        '''init weight size and value'''
-        if input is not None:
-            self._weight:np.ndarray = np.zeros(input.size[1], self._size)
-            self._bias:np.ndarray = np.zeros(self._size)
-
     @property
     def weight(self)->np.ndarray:
+        '''getter for layer weight'''
         return self._weight
 
     @property
     def bias(self)->np.ndarray:
+        '''getter for layer bias'''
         return self._bias
     
     @property
     def size(self)->int:
+        '''getter for layer size'''
         return self._size
+    
+    @property
+    def input_matrix(self) -> np.ndarray:
+        '''getter for input matrix'''
+        return self._input_matrix
+
+    @property
+    def activation(self) -> str:
+        '''getter for activation'''
+        return self._activation
 
     @weight.setter
     def weight(self, weight:np.ndarray)->None:
@@ -38,10 +46,47 @@ class DenseLayer():
     def bias(self, bias:np.ndarray) -> None:
         self._bias = bias
 
-    def sigmoid(self, z) -> np.float64:
-        return 1 / (1 + np.exp(-z))
+    @input_matrix.setter
+    def input_matrix(self, _input_matrix:np.ndarray) -> None:
+        self._input_matrix = _input_matrix
 
-    def output(self, )
+    def _sigmoid(self, z) -> np.float64:
+        '''sigmoid function. returns shape (sample_size,)'''
+        return 1 / (1 + np.exp(-z))
+    
+    def _sigmoid_derivative(self, y_predict:np.ndarray) -> np.ndarray:
+        return y_predict * (1 - y_predict)
+
+    def _softmax(self, z) -> np.ndarray:
+        '''sigmoid function. Returns shape (sample_size, size)'''
+        # to prevent overflow by shifting max value to 0 and every else to negative
+        z_exp = np.exp(z - np.max(z))
+        return z_exp / z_exp.sum(axis=-1, keepdims=True)
+
+    def y_predict(self) -> np.ndarray:
+        '''return z after activation function'''
+        z:np.ndarray = np.dot(self._input_matrix, self.weight) + self.bias
+        if len(self._weight) == 0:
+            return self.input_matrix
+        elif self._activation == "sigmoid":
+            return self._sigmoid(z)
+        elif self._activation == "softmax":
+            return self._softmax(z) 
+
+    def error_delta(self, y:np.ndarray) -> np.ndarray:
+        '''return error of layer'''
+        y_predict:np.ndarray = self.y_predict()
+        if self.activation == "softmax":
+            return y_predict[:,0] - y
+        if self.activation == "sigmoid":
+            return (y_predict - y) * self._sigmoid_derivative(self.y_predict())
+
+
+
+
+
+
+
     
 
     
--- mlp.py
+++ mlp.py
@@ -27,14 +27,89 @@ class MLPClassifier():
                 # Gaussian normal distribution of mean 0 and std deviation 1
                 layer.bias = rng.normal()
 
-    def _feedforward(self):
+    def _normalize(self, x:np.ndarray) -> np.ndarray:
+        '''convert each value to normalised Z score with mean of 0 and std deviation of 1'''
+        # normalise input
+        return ((x - np.mean(x,axis=0).reshape(1,-1)) / np.std(x,axis=0).reshape(1,-1))
+    
+    def _binary_cross_entropy_loss(self, y:np.ndarray, y_predict:np.ndarray)->np.ndarray:
+        '''calculate and return loss value'''
+        epsilon = 1e-10
+        # shape is (size of sample,)
+        # add epsilon to prevent log(0) which is undefined
+        return np.mean(-((y * np.log(y_predict + epsilon)) + \
+                        ((1 - y) * (np.log(1 - y_predict + epsilon)))))
+    
+    def shuffle(self, y:np.ndarray, X:np.ndarray, seed:int) -> None:
+        '''shuffle label and data in place according to seed'''
+        rng = np.random.default_rng(seed)
+        indices = np.arange(len(y))
+        rng.shuffle(indices)
+        return (y[indices], X[indices])
 
-    def fit(self, x_train:np.ndarray, x_valid:np.ndarray, y:np.ndarray, seed:int=42, learning_rate:float=0.01, iteration:int=100, batch_size:int=8, epoch:int=84)->tuple[list,list]:
+    def gradient(self, y:np.ndarray, y_predict:np.ndarray,X:np.ndarray,\
+                  sgd:bool) ->tuple[np.ndarray]:
+        '''calculate gradient of weight and bias'''
+        #dL/dy_predict = - ((y/y_predict) - (1-y/1-y_predict))
+        # dy_predict/dz = y_predict * (1 - y_predict)
+        # dz/dw
+        # gradient of loss w.r.t. dL/dw = (dL/dy_predict) x (dy_predict / dz) x (dz / dw)
+        # simplify = ((y_predict - y) * X) / sample_size.
+        # bias is the same except for dz/db = 1 hence (y_predict - y) / sample size
+        if sgd is False:
+            gradient_weight:np.ndarray = np.dot(X.T, y_predict - y) / len(y_predict)
+        else:
+            gradient_weight:np.ndarray = X * (y_predict - y)
+        gradient_bias:np.ndarray = np.mean(y_predict - y)
+        return (gradient_weight, gradient_bias)
+    
+    def _feedforward(self, x_shuffled:np.ndarray) -> None:
+        y_predict:np.ndarray = np.array([])
+        for _, layer in enumerate(self._layers):
+            layer.input_matrix = y_predict
+            y_predict = layer.y_predict(x_shuffled)
+    
+    def _sigmoid_derivative(self, y_predict:np.ndarray) -> np.ndarray:
+        return y_predict * (1 - y_predict)
+
+    def _backpropagate(self, y_batch:np.ndarray) -> None:
+        layer_error_delta:np.ndarray = np.array([])
+        for layer in reversed(list(enumerate(self._layers))):
+            if layer.activation == "softmax":
+                layer.weight -= layer.weight.T.dot(layer.error_delta(y_batch))
+                layer_error_delta = layer.error_delta(y_batch) 
+            elif layer.activation == "sigmoid":
+                layer.weight -= layer.weight.T.dot(layer_error_delta)
+                layer_error_delta = layer.error_delta()
+
+        
+
+    def fit(self, x_train:np.ndarray, x_valid:np.ndarray, y_train:np.ndarray, y_valid:np.ndarray, seed:int=42, early_stopping=False, learning_rate:float=0.01, batch_size:int=8, epoch:int=84)->tuple[list,list]:
         '''train model based on hyperparams'''
         print(f"x_train shape : {x_train.shape}")
         print(f"x_valid shape : {x_valid.shape}")
         self._init_weight(seed)
         self._init_bias(seed)
+        x_train = self._normalize(x_train)
+        x_valid = self._normalize(x_valid)
+        for i in range(epoch):
+            # For each Epoch shuffle training set(validation set don't require shuffling)
+            (y_shuffled, x_shuffled)=self.shuffle(y_train, x_train, seed)
+            start_index:int = 0
+            end_index:int = start_index + batch_size
+            while end_index < len(x_shuffled):
+                # mini batch SGD update every iteration (up to entire dataset for one epoch)
+                x_batch:np.ndarray = x_shuffled[start_index:end_index]
+                y_batch:np.ndarray = y_shuffled[start_index:end_index]
+                self._feedforward(x_batch)
+                self._backpropagate(y_batch)
+                # mini batch SGD backpropagate
+                gradient_weight, gradient_bias = self.gradient(y_batch, y_predict, X_batch, False)
+                self._weight -= self._learning_rate * gradient_weight
+                self._bias -= self._learning_rate * gradient_bias
+                start_index = end_index
+                end_index = start_index + self._batch_size
+
 
 
 
